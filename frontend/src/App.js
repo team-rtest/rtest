d4108e80d3414865007e58aacb9cee6bfd082a4f
jfrumk <jafrumkes@gmail.com>
2020-07-28 13:54:09 -0700

    Add Upload Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component{
+  onChangeHandler=event=>{
+    this.setState({ //Passes the selected file to the STATE
+    selectedFile: event.target.files[0],
+    loaded:0,
+    })
+  }
+  onClickHandler=()=>{
+    const data = new FormData()
+    data.append('file',this.state.selectedFile)//our selected file data
+    axios.post("http://localhost:3000/upload", data,{})//Send Post with endpoint URL and our form data with our file in it
+    .then(res => {//then we print the response status
+      console.log(res.statusText)
+    })
+  }
+  render() {
+    return (
+      //Code modified from https://bootsnipp.com/snippets/DOXy4
+      <div className = "container">
+        <div className="row">
+          <div className="col-md-12">
+              <form method="post" action="#" id="#">
+                    <div className="form-group files">
+                      <label>Upload Your File </label>
+                      <input type="file" name="file" className="form-control" multiple="" onChange = {this.onChangeHandler}/>
+                    </div>
+               </form>
+          </div>
+        </div>
+        <div className ="row">
+          <div className = "col-md-12">
+            <button type = "button" className="btn btn-primary btn-upload" onClick = {this.onClickHandler}>Upload</button>
+          </div>
+        </div>
+      </div>
   );
+  }
 }
 
 export default App;
