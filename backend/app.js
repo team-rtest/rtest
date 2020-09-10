fbfe2e2bc3057588b39b516fbb0078f1cbec42ad
Simon Marty <simon.marty0@gmail.com>
2020-09-10 12:23:34 -0400

    Add graphql endpoint (#13)

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var graphqlRouter = require('./routes/graphql');
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/graphql', graphqlRouter);
 
 module.exports = app;
