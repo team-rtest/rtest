768a6dbe5ca401bb3417d62c79b9365550aaad7f
Simon Marty <simon.marty0@gmail.com>
2020-09-11 13:53:04 -0400

    Fix graphql file read, expand schema

--- backend/routes/graphql.js
+++ backend/routes/graphql.js
@@ -1,13 +1,16 @@
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema, graphql } = require('graphql');
+const { readFileSync } = require('fs')
+const { resolve } = require('path')
 var express = require('express');
 var router = express.Router();
 
 // Construct a schema, using GraphQL schema language
 
+const typeDefs = readFileSync(resolve(__dirname, "../schemas/schema.graphql"), "utf8");
 // TODO Feed schema.graphql to this
-var schema = buildSchema();
-
+var schema = buildSchema(typeDefs);
+ 
 // The root provides a resolver function for each API endpoint
 var root = {
   hello: () => {
