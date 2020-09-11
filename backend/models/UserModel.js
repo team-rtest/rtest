9163029bdefc50b27cdf0e52f1305a30bf228a98
Simon Marty <simon.marty0@gmail.com>
2020-09-11 16:51:29 -0400

    Fix style

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,5 +1,5 @@
-import bcrypt from 'bcrypt';
-import mongoose from 'mongoose';
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   id: {
@@ -14,16 +14,16 @@ const userSchema = new mongoose.Schema({
   posts: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'post',
+      ref: "post",
     },
   ],
 });
 
-userSchema.pre('save', function() {
+userSchema.pre("save", function() {
   const hashedPassword = bcrypt.hashSync(this.password, 12);
   this.password = hashedPassword;
 });
 
-const user = mongoose.model('user', userSchema);
+const user = mongoose.model("user", userSchema);
 
 export default user;
--- backend/resolvers/UserResolvers.js
+++ backend/resolvers/UserResolvers.js
@@ -1,11 +1,11 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export default {
   Query: {
     user: async (parent, { id }, { models: { userModel }, me }, info) => {
       if (!me) {
-        throw new Error('You are not authenticated');
+        throw new Error("You are not authenticated");
       }
       const user = await userModel.findById({ _id: id }).exec();
       return user;
@@ -14,20 +14,18 @@ export default {
       const user = await userModel.findOne({ name }).exec();
 
       if (!user) {
-        throw new Error('Invalid credentials');
+        throw new Error("Invalid credentials");
       }
 
       const matchPasswords = bcrypt.compareSync(password, user.password);
 
       if (!matchPasswords) {
-        throw new Error('Invalid credentials');
+        throw new Error("Invalid credentials");
       }
 
-      const token = jwt.sign({ id: user.id }, 'riddlemethis', { expiresIn: 24 * 10 * 50 });
+      const token = jwt.sign({ id: user.id }, "riddlemethis", { expiresIn: 24 * 10 * 50 });
 
-      return {
-        token,
-      };
+      return token;
     },
   },
   Mutation: {
