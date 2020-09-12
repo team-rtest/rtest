bafb115012cff40ab8d8e833cc492836e8a708a8
Simon Marty <simon.marty0@gmail.com>
2020-09-11 22:14:36 -0400

    add temp model and resolver

---
+++ backend/models/UserModel.js
@@ -0,0 +1,29 @@
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ],
+});
+
+userSchema.pre("save", function() {
+  const hashedPassword = bcrypt.hashSync(this.password, 12);
+  this.password = hashedPassword;
+});
+
+const user = mongoose.model("user", userSchema);
+
+export default user;
