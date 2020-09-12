9a08c2fe11dfc9881a9be10dd3f00e113c25bf55
Simon Marty <simon.marty0@gmail.com>
2020-09-11 20:45:33 -0400

    Remove unnecessary routers

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var graphqlRouter = require("./routes/graphql");
 
 var app = express();
@@ -15,8 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/graphql", graphqlRouter);
 
 module.exports = app;
