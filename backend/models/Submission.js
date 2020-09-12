4516232bf34ee29122a43f37150b0d3cd38dad34
Simon Marty <simon.marty0@gmail.com>
2020-09-12 12:23:33 -0400

    Denormalize Grade, add File

---
+++ backend/models/Submission.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const submissionSchema = new mongoose.Schema({
+  _id: {
+    type: ObjectId,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    ref: "user",
+    required: true,
+  },
+  assignment: {
+    type: ObjectId,
+    ref: "assignment",
+    required: true,
+  },
+  files: {},
+  submittedAt: {
+    type: Date,
+    required: true,
+  },
+  grade: {
+    type: Double,
+    required: true,
+  },
+});
+
+const submission = mongoose.model("submission", submissionSchema);
+
+export default submission;
