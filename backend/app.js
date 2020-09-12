d21269b3a2778bc1af6c276109b5c8aca9cf40f0
Simon Marty <simon.marty0@gmail.com>
2020-09-11 23:07:42 -0400

    Add GraphQL schema, CommonJS to ES6 (#28)
    * Remove unnecessary files, rename www
    
    * add temp model and resolver
    
    * Add graphql stuff
    
    * Cleanup commonjs to ES6
    
    * Remove test.js
    
    * Fix style issues
    
    * Revert function move

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import graphqlRouter from "./routes/graphql";
+import compression from "compression";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var graphqlRouter = require('./routes/graphql');
-
-var app = express();
-
-app.use(logger('dev'));
+const app = express();
+app.use(compression());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/graphql', graphqlRouter);
+app.use("/graphql", graphqlRouter);
 
-module.exports = app;
+export default app;
---
+++ backend/db.js
@@ -0,0 +1,6 @@
+import defaultExport from 'resolvers/UserResolvers';
+import defaultExport from 'models/UserModel';
+
+const db = UserModel();
+
+export default db;
