005f1378059c014566f9e95e7615fbc20910325a
Simon Marty <simon.marty0@gmail.com>
2020-09-13 17:34:54 -0400

    Add passport

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,10 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import graphqlRouter from "./routes/graphql";
 import compression from "compression";
+import passport from "passport";
 
 const app = express();
+app.use(passport.initialize());
 app.use(compression());
 app.use(logger("dev"));
 app.use(express.json());
