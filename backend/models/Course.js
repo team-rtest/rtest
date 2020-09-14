99fcebb32987fafca0b4481b2e5b3e34789d38d5
Simon Marty <simon.marty0@gmail.com>
2020-09-14 18:48:31 +0000

    Connect GraphQL to Mongo (#29)
    * Add property shorthand
    
    * Expand graphql, advance on user
    
    * Add files for models
    
    * Finish user.js
    
    * Fix typo
    
    * Add grade schema
    
    * Denormalize Grade, add File
    
    * Add file to submission
    
    * Add assignment mongoose schema
    
    * Fix tags value
    
    * Change Class to Course
    
    * Add sample test
    
    * Add simple graphql test
    
    * Fix style issues
    
    * refine schemas
    
    * Add timeGraded to peergrades
    
    * ObjectId to mongoose.Schema.Types.ObjectId
    
    * Fix data types
    
    * Int -> Number
    
    * Update yarn.lock
    
    * Add passport
    
    * Revert "Add passport"
    
    This reverts commit 005f1378059c014566f9e95e7615fbc20910325a.
    
    Co-authored-by: jfrumk <jafrumkes@gmail.com>

---
+++ backend/models/Course.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+
+  courseCode: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  courseNumber: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  semester: {
+    type: String,
+    required: true,
+  },
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  assignments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "assignment",
+    },
+  ],
+});
+
+const course = mongoose.model("course", courseSchema);
+export default course;
