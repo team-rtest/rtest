88223ad9899daab0c88ca7d089669e2ea05056f2
Simon Marty <simon.marty0@gmail.com>
2020-09-17 18:28:03 -0400

    Finish fixing Yohan's branch

--- frontend/src/views/Auth/Signup.js
+++ frontend/src/views/Auth/Signup.js
@@ -1,49 +1,56 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import { Form, Input, Card } from 'components'
-import { AuthCard, AuthForm, Heading, Link } from './styles'
+import { Form, Input, Card, Textarea } from "components";
+import { AuthCard, AuthForm, Heading, AuthLink } from "./styles";
+
+import { auth } from "api";
 
 function Signup() {
-  const [inputs, setInputs] = useState({ email: '', password: '' });
+  const [inputs, setInputs] = useState({ email: "", password: "" });
   const [errors, setErrors] = useState({ email: null, password: null });
 
   const handleChange = (name, value) => {
     setInputs({ ...inputs, [name]: value });
     setErrors({ ...errors, [name]: validate(name, value) });
-  }
+  };
 
   const validate = (name, value) => {
-    switch(name) {
-      case 'email':     return validateEmail(value);
-      case 'password':  return validatePassword(value);
-      default:          return !value;
+    switch (name) {
+      // case 'email':     return validateEmail(value);
+      case "password":
+        return validatePassword(value);
+      default:
+        return !value;
     }
-  }
+  };
 
-  const validateEmail = value => {
+  const validateEmail = (value) => {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const emailValid = emailRegex.test(value.toLowerCase())
-    if(!emailValid) return 'Email is invalid';
-  }
+    const emailValid = emailRegex.test(value.toLowerCase());
+    if (!emailValid) return "Email is invalid";
+  };
 
-  const validatePassword = value => {
-    if(value.length < 8) return 'Password must have at least 8 characters';
-  }
+  const validatePassword = (value) => {
+    if (value.length < 8) return "Password must have at least 8 characters";
+  };
 
   const handleSubmit = () => {
     setErrors({
-      email: validateEmail(inputs.email),
+      // email: validateEmail(inputs.email),
       password: validatePassword(inputs.password),
     });
 
     const noneEmpty = inputs.email && inputs.password;
     const noneError = !errors.email && !errors.password;
 
-    if(noneEmpty && noneError) {
-      alert('form submitted successfully!');
+    if (noneEmpty && noneError) {
+      alert("form submitted successfully!");
+      const res = auth.signup(inputs.email, inputs.password);
+      // TODO handle failure
+      console.log(res);
     }
-  }
+  };
 
   return (
       <AuthCard>
