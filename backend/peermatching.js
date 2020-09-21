84adc9ad08628fed05ef94b17d380a51c07b2e38
Simon Marty <simon.marty0@gmail.com>
2020-09-21 03:42:04 +0000

    Add semicolons

--- backend/peermatching.js
+++ backend/peermatching.js
@@ -1,10 +1,10 @@
 function matchpeer(questions, students, numTask) {
     shuffle(questions);
     shuffle(students);
-    var len = questions.length
+    var len = questions.length;
     for (var i=0; i < students.length; i++) {
         for ( var j=0; j < len; j++) {
-            addPeer(questions[(j + i)%len],students[i])
+            addPeer(questions[(j + i)%len],students[i]);
         }
     }
 }
