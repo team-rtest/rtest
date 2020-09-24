f7d7cc51de64fb7df340aa8a124a1957fac54a5d
Simon Marty <simon.marty0@gmail.com>
2020-09-23 21:42:18 -0400

    Remove react-cookie

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { CookiesProvider } from "react-cookie";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
