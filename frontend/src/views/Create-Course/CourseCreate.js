fb315433192e218334d7ed76e45ad8b84d94cb80
jfrumk <jafrumkes@gmail.com>
2020-09-27 13:16:50 -0400

    Add create course button

--- frontend/src/views/Create-Course/CourseCreate.js
+++ frontend/src/views/Create-Course/CourseCreate.js
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 import ClassCard from './ClassCard';
 import { Card } from 'components';
 
 function CourseCreate({ selected, setSelected, assignments }) {
-
+  const PLUS = <Plus className = {'fa fa-plus-circle'}/>;
   const courses = [
-    { id: 'CS470-001', name: 'Data Mining', bgcolor:'#f2b3e5' },
-    { id: 'CS325-002', name: 'Artificial Intelligence', bgcolor:'#c1f7e5' },
-    { id: 'CS334-001', name: 'Machine Learning', bgcolor:'#eef5c4' },
-    { id: 'CS377-001', name: 'Database Systems', bgcolor:'#cfc4f5' },
+    { id: 'CS470-001', name: 'Data Mining', bgcolor:'#32a852' },
+    { id: 'CS325-002', name: 'Artificial Intelligence', bgcolor:'#7732a8' },
+    { id: 'CS334-001', name: 'Machine Learning', bgcolor:'#a83246' },
+    { id: 'CS377-001', name: 'Database Systems', bgcolor:'#eba226' },
   ]
 
   return (
     <Dashboard>
       <Heading>Create or Review Courses</Heading>
-      <Grid>{ courses.map(course => <ClassCard {...course} />) }<AddClass /></Grid>
+  <Grid>{ courses.map(course => <ClassCard {...course} />) }<Link to ='course-create/form' ><AddClass><h4>{PLUS} Add Class</h4></AddClass></Link></Grid> 
     </Dashboard>
   );
 }
@@ -32,9 +33,25 @@ const Heading = styled.h1`
   margin-bottom: 25px;
 `;
 
-const AddClass = styled(Card)`
+const AddClass = styled.button`
+background-color: #f8f9fa;
+color: rgba(0,0,0,0.75);
+font-weight: 600;
+margin-bottom: 0;
+padding: 20px;
+align-items: center;
+height: 10rem;
+border-radius: 4px;
+border-color:rgb(217,218,219);
+border-style: solid;
+border-width: 2px;
+width: 20rem;
 `;
-
+const Plus = styled.span`
+  font-size: 1.5rem;
+  color: black;
+  opacity: 0.75;
+`;
 const Grid = styled.div`
   display: grid;
   grid-template-columns: 325px 325px 325px;
