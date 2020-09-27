3d14eb0944fe9efc0417ca9d7597d98067778079
Simon Marty <simon.marty0@gmail.com>
2020-09-27 13:44:13 -0400

    Simplify course schema

--- backend/models/Course.js
+++ backend/models/Course.js
@@ -1,36 +1,22 @@
 import mongoose from "mongoose";
 
 const courseSchema = new mongoose.Schema({
-  _id: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  subject: {
-    type: String,
-    required: true,
-  },
-  courseNumber: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: Number,
-    required: true,
-  },
-  semester: {
-    type: String,
-    required: true,
-  },
-  section: {
-    type: Number,
-    required: true,
-  },
-  members: [
+  subject: String,
+  courseNumber: String,
+  year: Number,
+  semester: String,
+  sections: [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
+      section: Number,
+      members: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "user",
+        },
+      ],
     },
   ],
+
   assignments: [
     {
       type: mongoose.Schema.Types.ObjectId,
