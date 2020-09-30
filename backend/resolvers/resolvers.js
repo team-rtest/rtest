7ca1e48725c6143852cb35dd8ee7c00f177b5017
Simon Marty <simon.marty0@gmail.com>
2020-09-30 03:20:58 -0400

    Fix CreateCourse mutation!

--- backend/resolvers/resolvers.js
+++ backend/resolvers/resolvers.js
@@ -1,44 +1,44 @@
 import Course from "../models/Course";
-import Assignment from "../models/Assignment";
-import Submission from "../models/Submission";
+// import Assignment from "../models/Assignment";
+// import Submission from "../models/Submission";
 
 export const resolvers = {
   Query: {
     hello: () => "hi",
-    course: async (parent, { id }, { models: { Course }, me }, info) => {
+    course: async () => {
       const course = await Course.findbyId({ _id: id }).exec();
       return course;
     },
     courses: async () => Course.find().exec(),
-    submission: async (parent, { id }, { model: { Submission }, me }, info) => {
-      const submission = await Submission.findById({ _id: id }).exec();
-      return submission;
-    },
-    assignment: async (
-      parent,
-      { id },
-      { models: { Assignments }, me },
-      info
-    ) => {
-      const assignment = await Assignments.findById({ _id: id }).exec();
-      return assignment;
-    },
+    // submission: async () => {
+    //   const submission = await Submission.findById({ _id: id }).exec();
+    //   return submission;
+    // },
+    // assignment: async (
+    //   parent,
+    //   { id },
+    //   { models: { Assignments }, me },
+    //   info
+    // ) => {
+    //   const assignment = await Assignments.findById({ _id: id }).exec();
+    //   return assignment;
+    // },
   },
   Mutation: {
-    createCourse: async (courseInput) => {
-      const course = new Course(courseInput);
-      await course.save();
-      return course;
+    createCourse: async (_, { course }) => {
+      const c = new Course(course);
+      await c.save();
+      return c;
     },
-    createAssignment: async (
-      parent,
-      assignmentInput,
-      { models: Assignment },
-      info
-    ) => {
-      const assignment = new Assignment(assignmentInput);
-      await assignment.save();
-      return assignment;
-    },
+    // createAssignment: async (
+    //   parent,
+    //   assignmentInput,
+    //   { models: Assignment },
+    //   info
+    // ) => {
+    //   const assignment = new Assignment(assignmentInput);
+    //   await assignment.save();
+    //   return assignment;
+    // },
   },
 };
