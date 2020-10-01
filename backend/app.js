d60bcda8fe6f6ae0c8437fab6943f504a024f3ed
Simon Marty <simon.marty0@gmail.com>
2020-09-30 20:31:22 -0400

    Add google oauth (identity token verification) on the backend

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,13 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import graphqlRouter from "./routes/graphql.js";
 import User from "./models/User.js";
-import { generateToken } from "./auth.js";
+import { generateToken, verifyGoogleToken } from "./auth.js";
 import compression from "compression";
 import passport from "passport";
 import mongoose from "mongoose";
 import cors from "cors";
 import csrf from "csurf";
+import bearerToken from "express-bearer-token";
 
 if (!process.env.JEST_WORKER_ID) {
   mongoose.connect(
@@ -83,20 +84,13 @@ app.post("/login", passport.authenticate("local"), (req, res) => {
   res.json({ status: "Successfully Logged In" });
 });
 
-if (process.env.GOOGLE_CLIENT_ID) {
-  app.get(
-    "/auth/google",
-    passport.authenticate("google", { scope: ["profile"] })
-  );
-
-  app.get(
-    "/auth/google/callback",
-    passport.authenticate("google", { failureRedirect: "/login" }),
-    function (req, res) {
-      res.redirect("/");
-    }
-  );
-}
+app.get("/auth/google", csrfProtection, bearerToken(), (req, res) => {
+  verifyGoogleToken(req.token)
+    .then((payload) => res.send(payload)) // TODO do some other stuff here, create a user etc
+    .catch(() =>
+      res.json({ status: "Could not verify your OAuth ID with Google" })
+    );
+});
 
 app.use("/graphql", csrfProtection, graphqlRouter);
 
