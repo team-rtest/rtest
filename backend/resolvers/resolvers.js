edfd4e9c92edd9df87456a5ad4e5bb5dd7e3b5a8
Simon Marty <simon.marty0@gmail.com>
2020-10-02 18:32:38 -0400

    Combine student and instructor into user

--- backend/resolvers/resolvers.js
+++ backend/resolvers/resolvers.js
@@ -15,41 +15,23 @@ export const resolvers = {
       const course = await Course.findbyId({ _id: id }).exec();
       return course;
     },
-    student: async(_, { id }) =>{
-      const s = await User.findbyId({_id: id}).exec();
+    user: async (_, { id }) => {
+      const s = await User.findbyId({ _id: id }).exec();
       return s;
     },
-    instructor: async(_, { id }) =>{
-      const i = await User.findbyId({_id: id}).exec();
-      return i;
-    },
-    assignment: async (_, { id }) =>{
-      const a = await Assignment.findbyId({_id: id}).exec();
+    assignment: async (_, { id }) => {
+      const a = await Assignment.findbyId({ _id: id }).exec();
       return a;
     },
-    submission: async(_, { id }) =>{
-      const su = await Submission.findbyId({_id: id}).exec();
+    submission: async (_, { id }) => {
+      const su = await Submission.findbyId({ _id: id }).exec();
       return su;
     },
 
-
     courses: async () => Course.find().exec(),
-    student: async() => User.find().exec(),
-    instructor: async() => User.find().exec(),
-    assignment: async() => Assignment.find().exec(),
-    // submission: async () => {
-    //   const submission = await Submission.findById({ _id: id }).exec();
-    //   return submission;
-    // },
-    // assignment: async (
-    //   parent,
-    //   { id },
-    //   { models: { Assignments }, me },
-    //   info
-    // ) => {
-    //   const assignment = await Assignments.findById({ _id: id }).exec();
-    //   return assignment;
-    // },
+    users: async () => User.find().exec(),
+    assignments: async () => Assignment.find().exec(),
+    submissions: async () => Submission.find().exec(),
   },
   Mutation: {
     createCourse: async (_, { course }) => {
@@ -57,32 +39,15 @@ export const resolvers = {
       await c.save();
       return c;
     },
-    createStudent: async(_, { student }) => {
-      const s = new User(student);
+    createUser: async (_, { user }) => {
+      const s = new User(user);
       await s.save();
       return s;
     },
-    createInstructor: async(_, { instructor }) => {
-      const i = new User(instructor);
-      await i.save();
-      return i;
-    },
-    createAssignment: async(_, { assignment }) =>{
+    createAssignment: async (_, { assignment }) => {
       const a = new Assignment(assignment);
       await a.save();
       return a;
-    }
-    
-
-    // createAssignment: async (
-      //   assignmentInput,
-      //   parent,
-    //   { models: Assignment },
-    //   info
-    // ) => {
-    //   const assignment = new Assignment(assignmentInput);
-    //   await assignment.save();
-    //   return assignment;
-    // },
+    },
   },
 };
