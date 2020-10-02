f1386500115a298d0f579571dbb058cf55a39eaf
Simon Marty <simon.marty0@gmail.com>
2020-10-02 19:52:28 -0400

    Update mutations and set Luis up for easier work

--- backend/resolvers/resolvers.js
+++ backend/resolvers/resolvers.js
@@ -5,8 +5,7 @@ import { async } from "regenerator-runtime/runtime";
 import Assignment from "../models/Assignment";
 import Submission from "../models/Submission";
 import User from "../models/User";
-// import Assignment from "../models/Assignment";
-// import Submission from "../models/Submission";
+import AssignmentGroup from "../models/AssignmentGroup";
 
 export const resolvers = {
   Query: {
@@ -39,15 +38,56 @@ export const resolvers = {
       await c.save();
       return c;
     },
+
     createUser: async (_, { user }) => {
       const s = new User(user);
       await s.save();
       return s;
     },
+
+    // TODO the schema is currently not passing the stuff you actually need, make sure to
+    // update the input types as needed for your resolvers
+    // (This is my fault, I updated the resolvers; I'm not flaming you) - Simon
+
+    createAssignmentGroup: async (_, { assignmentGroup }) => {
+      const ag = new AssignmentGroup(assignmentGroup);
+      await ag.save();
+      // TODO I'm not sure this is correct, read up on how updateOne works and if it needs
+      // to be waited on with async, or if it needs exec()
+      await Course.updateOne(
+        { id: assignmentGroup.courseId },
+        { $push: { assignmentGroups: ag._id } }
+      );
+      return ag;
+    },
+
     createAssignment: async (_, { assignment }) => {
       const a = new Assignment(assignment);
       await a.save();
+      await AssignmentGroup.updateOne(
+        { _id: assignment.assignmentGroupId },
+        { $push: { assignments: a._id } }
+      );
       return a;
+    },
+
+    createSubmission: async (_, { submission }) => {
+      const s = new Submission(submission);
+      await s.save();
+      await Assignment.updateOne(
+        { _id: submission.assignmentId },
+        { $push: { submissions: s._id } }
+      );
+    },
+
+    updateGrade: async (_, { gradeInput }) => {
+      return gradeInput; // TODO
+    },
+
+    peerGradeSubmission: async (_, { peerGradeInput }) => {
+      // creates a new peer grade if the grader hasn't graded the assignment yet
+      // otherwise updates the student's grade
+      return peerGradeInput; // TODO
     },
   },
 };
