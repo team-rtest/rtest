2203e9eb859d3b4e0cebcb0e6d040b8b92be2b12
Simon Marty <simon.marty0@gmail.com>
2020-10-04 15:29:29 -0400

    Fix test

--- frontend/src/views/Review/AssignmentReview.test.js
+++ frontend/src/views/Review/AssignmentReview.test.js
@@ -1,38 +1,37 @@
-import getGradeList from "AssignmentReviewHelper";
+import { getGradeList } from "./AssignmentReviewHelper";
 
-test('should return a list of grades', ()=>{
-    const submissions =[
-        {
-          student: {
-              firstName: "Jack",
-              lastName: "Frumkes"
-          },
-          files: [],
-          submittedAt: 1601484990,
-          grade: 87,
-          peerGrades: []
-        },
-        {
-          student: {
-              firstName: "Simon",
-              lastName: "Marty"
-          },
-          files: [],
-          submittedAt: 1601484990,
-          grade: 100,
-          peerGrades: []
-        },
-        {
-          student: {
-              firstName: "Luis",
-              lastName: "Flores"
-          },
-          files: [],
-          submittedAt: 1601484981,
-          grade: 91,
-          peerGrades: []
-        }
-      ]
-    const grades = getGradeList(submissions)
-    expect([87,100,91])
-})
+test("should return a list of grades", () => {
+  const submissions = [
+    {
+      student: {
+        firstName: "Jack",
+        lastName: "Frumkes",
+      },
+      files: [],
+      submittedAt: 1601484990,
+      grade: 87,
+      peerGrades: [],
+    },
+    {
+      student: {
+        firstName: "Simon",
+        lastName: "Marty",
+      },
+      files: [],
+      submittedAt: 1601484990,
+      grade: 100,
+      peerGrades: [],
+    },
+    {
+      student: {
+        firstName: "Luis",
+        lastName: "Flores",
+      },
+      files: [],
+      submittedAt: 1601484981,
+      grade: 91,
+      peerGrades: [],
+    },
+  ];
+  expect(getGradeList(submissions)).toStrictEqual([87, 100, 91]);
+});
