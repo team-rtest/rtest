58db3bd90444879e3017b5765632aee2ba12879f
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-10-05 18:29:09 +0530

    Fixed test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-// test('renders upload button', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/Your Courses/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+import Courses from 'views/Professor/Courses/Courses';
+
+test('renders your courses', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Your Courses/i);
+  expect(linkElement).toBeInTheDocument();
+});
