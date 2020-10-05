5648804a8b0f0ed6e3e7523f6f95026ced7dee46
Simon Marty <simon.marty0@gmail.com>
2020-10-05 10:15:12 -0400

    Type to tag

--- backend/models/AssignmentGroup.js
+++ backend/models/AssignmentGroup.js
@@ -5,7 +5,7 @@ const assignmentGroupSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  type: {
+  Tag: {
     type: String,
     required: true,
     enum: ["HW", "TEST", "PEER"],
