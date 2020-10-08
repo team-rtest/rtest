8d0f29796c58602e678f6f82dfa36d8ddaa0b2bc
Simon Marty <simon.marty0@gmail.com>
2020-10-08 18:09:24 -0400

    Add username from jwt to context

--- backend/src/routes/graphql.js
+++ backend/src/routes/graphql.js
@@ -1,11 +1,26 @@
 import { readFileSync } from "fs";
 import { resolvers } from "../resolvers/resolvers.js";
-import pkg from 'apollo-server-express';
+import pkg from "apollo-server-express";
+import { getUsernameFromToken } from "../auth/auth.js";
 const { ApolloServer } = pkg;
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = readFileSync("./src/schemas/schema.graphql", "utf8");
 // const schema = buildSchema(typeDefs);
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    //req.headers.authorization
+    const token = req.token || "";
+    const user = "noauth";
+    if (token) {
+      user = getUsernameFromToken(token);
+    }
+    
+    // TODO deseralize token to username
+    return { user };
+  },
+});
 export default server;
