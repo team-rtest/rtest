0675403ba77224c377015ea8c9fc33b9833c66e0
Simon Marty <simon.marty0@gmail.com>
2020-10-08 10:10:30 -0400

    Add some helper methods

---
+++ auth/userAuthUtil.js
@@ -0,0 +1,17 @@
+export default {
+  isInClass: (username) => {
+    return true;
+  },
+  ownsSubmission: (username) => {
+    return true;
+  },
+  isClassInstructor: (username) => {
+    return true;
+  },
+  canGradeAssignment: (username) => {
+    return true;
+  },
+  canPeerReviewAssignment: (username) => {
+    return true;
+  },
+};
