15983ca0b9f36800c2e09972179de5c2192b4fbf
jfrumk <jafrumkes@gmail.com>
2020-10-17 12:44:37 -0400

    await updateGrade

--- backend/src/resolvers/submissionResolvers.js
+++ backend/src/resolvers/submissionResolvers.js
@@ -24,7 +24,7 @@ export default {
     },
 
     updateGrade: async (_, { submission, grade }) => {
-      Submission.updateOne(
+      await Submission.updateOne(
         { _id: mongoose.Types.ObjectId(submission) },
         { $set: { grade: grade } }
       );
