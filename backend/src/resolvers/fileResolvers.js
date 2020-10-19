67f9d5cb982328138e9f7cc0b6463fa6ec5fd940
Simon Marty <simon.marty0@gmail.com>
2020-10-19 01:29:55 -0400

    Add resolver to add file to submission

--- backend/src/resolvers/fileResolvers.js
+++ backend/src/resolvers/fileResolvers.js
@@ -2,7 +2,10 @@ import {
   getPresignedUpload,
   getPresignedDownload,
   uploadSubmission,
+  getFileMetadata,
 } from "../routes/fileHandler.js";
+import File from "../models/File.js";
+import Submission from "../models/Submission.js";
 
 export default {
   Query: {
@@ -13,12 +16,28 @@ export default {
       return await getPresignedDownload(bucket, key);
     },
   },
-  Mutation :{
+  Mutation: {
     addSubmissionFile: async (
       _,
       { course, assignment, submission, filename }
     ) => {
       return await uploadSubmission(course, assignment, submission, filename);
+    },
+    registerSubmissionFile: async (_, { submission, file }) => {
+      const metadata = await getFileMetadata(file.bucket, file.key);
+      if (!metadata) {
+        return;
+      }
+
+      const f = new File({
+        ...file,
+        size: metadata.ContentLength,
+        mimetype: metadata.ContentType,
+      });
+
+      await f.save();
+      await Submission.findByIdAndUpdate(submission, { $push: { files: f } });
+      return f;
     },
   },
 };
