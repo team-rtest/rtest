e21113cf24583fd90acdce3961469fadc3f37a35
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-10-20 00:54:30 +0530

    Delete App.test.js

--- frontend/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders your courses", () => {
-  const { getByText } = render(<App />);
-  const text = getByText("Hello World");
-  expect(text).toBeInTheDocument();
-});
