6fcf74155e1fb8e7cfb4433bc63493da6ca4fbd2
rhcheun <70978694+rhcheun@users.noreply.github.com>
2020-10-25 20:57:33 -0400

    Upload retrieve (#119)
    * upload path query
    
    * upload query
    
    * Fix Roland's code
    
    Co-authored-by: Roland <rhcheun@emory.edu>
    Co-authored-by: Simon Marty <simon.marty0@gmail.com>

--- frontend/src/api/fileUpload.js
+++ frontend/src/api/fileUpload.js
@@ -8,3 +8,24 @@ export const uploadSubmissionFile = (course, assignment, submission, filename) =
     })
     .then((result) => console.log(result));
 };
+
+export const geturl = gql`
+        query getPresigned($key: String!, $bucket: String!) { getPresignedUpload(key: $key, bucket: $bucket)}
+`;
+
+export const getUploadPath = gql`
+  query {
+    course {
+      id
+      assignments {
+        name
+        submissions {
+          user {
+            firstName
+            lastName
+          }
+        }
+      }
+    }
+  }
+`;
