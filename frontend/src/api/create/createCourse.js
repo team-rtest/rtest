74dbe8e885fc668c4da988fe69e98a6187dabe33
Simon Marty <simon.marty0@gmail.com>
2020-10-28 16:51:25 -0400

    Course -> CourseInput

--- frontend/src/api/create/createCourse.js
+++ frontend/src/api/create/createCourse.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export default gql`
-  mutation CreateCourse($course: Course) {
+  mutation CreateCourse($course: CourseInput) {
     createCourse(course: $course) {
       _id
     }
