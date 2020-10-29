a7edd682db291b6a197b7afc86bc54f5e38910b8
Simon Marty <simon.marty0@gmail.com>
2020-10-29 02:29:53 +0000

    Big resolver clean up (#150)
    * Fix token error
    
    * Add student query
    
    * Update dependabot.yml
    
    * Update dockerfile for frontend: add healthcheck and build stage
    
    * Add login and signup resolvers (#148)
    
    * Bump graphql from 15.3.0 to 15.4.0 in /frontend (#145)
    
    Bumps [graphql](https://github.com/graphql/graphql-js) from 15.3.0 to 15.4.0.
    - [Release notes](https://github.com/graphql/graphql-js/releases)
    - [Commits](https://github.com/graphql/graphql-js/compare/v15.3.0...v15.4.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump aws-sdk from 2.779.0 to 2.780.0 in /backend (#146)
    
    Bumps [aws-sdk](https://github.com/aws/aws-sdk-js) from 2.779.0 to 2.780.0.
    - [Release notes](https://github.com/aws/aws-sdk-js/releases)
    - [Changelog](https://github.com/aws/aws-sdk-js/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/aws/aws-sdk-js/compare/v2.779.0...v2.780.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump mongoose from 5.10.9 to 5.10.11 in /backend (#143)
    
    Bumps [mongoose](https://github.com/Automattic/mongoose) from 5.10.9 to 5.10.11.
    - [Release notes](https://github.com/Automattic/mongoose/releases)
    - [Changelog](https://github.com/Automattic/mongoose/blob/master/History.md)
    - [Commits](https://github.com/Automattic/mongoose/compare/5.10.9...5.10.11)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump google-auth-library from 6.1.2 to 6.1.3 in /backend (#141)
    
    Bumps [google-auth-library](https://github.com/googleapis/google-auth-library-nodejs) from 6.1.2 to 6.1.3.
    - [Release notes](https://github.com/googleapis/google-auth-library-nodejs/releases)
    - [Changelog](https://github.com/googleapis/google-auth-library-nodejs/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/googleapis/google-auth-library-nodejs/compare/v6.1.2...v6.1.3)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Remove automerge
    
    * Update dependabot.yml
    
    * Bump @testing-library/jest-dom from 5.11.4 to 5.11.5 in /frontend (#138)
    
    Bumps [@testing-library/jest-dom](https://github.com/testing-library/jest-dom) from 5.11.4 to 5.11.5.
    - [Release notes](https://github.com/testing-library/jest-dom/releases)
    - [Changelog](https://github.com/testing-library/jest-dom/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/testing-library/jest-dom/compare/v5.11.4...v5.11.5)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Update dependabot.yml
    
    * Update dependabot.yml
    
    * Extract frontend URL as env variable for backend (#149)
    
    * Bump jest from 26.6.0 to 26.6.1 in /backend (#135)
    
    Bumps [jest](https://github.com/facebook/jest) from 26.6.0 to 26.6.1.
    - [Release notes](https://github.com/facebook/jest/releases)
    - [Changelog](https://github.com/facebook/jest/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/facebook/jest/compare/v26.6.0...v26.6.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Big resolver clean up
    
    Co-authored-by: jfrumk <jafrumkes@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- backend/src/resolvers/assignmentResolvers.js
+++ backend/src/resolvers/assignmentResolvers.js
@@ -4,16 +4,6 @@ import AssignmentGroup from "../models/AssignmentGroup.js";
 import Submission from "../models/Submission.js";
 
 export default {
-  Query: {
-    assignment: async (_, { id }) => {
-      return await Assignment.findById(id);
-    },
-
-    assignments: async () => {
-      return await Assignment.find();
-    },
-  },
-
   Assignment: {
     submissions: async (assignment) => {
       return await Submission.find({
@@ -23,11 +13,11 @@ export default {
   },
 
   Mutation: {
-    createAssignment: async (_, { assignment }) => {
+    createAssignment: async (_, { assignmentGroup, assignment }) => {
       const a = new Assignment(assignment);
       await a.save();
       await AssignmentGroup.updateOne(
-        { _id: mongoose.Types.ObjectId(assignment.assignmentGroupId) },
+        { _id: mongoose.Types.ObjectId(assignmentGroup) },
         { $addToSet: { assignments: a._id } }
       );
       return a;
