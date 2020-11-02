f1b2a081237ac2f994f26f464ac8cd732282313d
Roland <rhcheun@emory.edu>
2020-11-02 00:59:14 -0500

    update

--- backend/src/resolvers/submissionResolvers.js
+++ backend/src/resolvers/submissionResolvers.js
@@ -19,16 +19,6 @@ export default {
       await Submission.updateOne({ _id: mongoose.Types.ObjectId(id) }, { $set: { grade } });
       return grade;
     },
-    // peerGradeSubmission: async (_, { peerGradeInput }) => {
-    //   creates a new peer grade if the grader hasn't graded the assignment yet
-    //   otherwise updates the student's grade
-    //   return peerGradeInput; // TODO
-    //}
-    peerGradeSubmission: async (_, { id, grader, grade }) => {
-      await Submission.updateOne({ _id: mongoose.Types.ObjectId(id), "peerGrades.grader": grader },
-      { $set: { grade } });
-      return grade;
-    },
 
     updateSubmission: async (_, { id, submission }) => {
       return await Submission.findByIdAndUpdate(id, submission);
