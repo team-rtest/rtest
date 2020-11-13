9a1730f0d355bdcb5478ac15e2c1f55bc9a09980
gluisjosue <gluisjosue@gmail.com>
2020-11-13 10:02:50 -0600

    add review page to course

---
+++ frontend/src/views/Internal/Professor/Course/Review/AssignmentReview.js
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "styled-components";
+import {
+  getGradeList,
+  getMedian,
+  getMax,
+  getMin,
+} from "./AssignmentReviewHelper";
+import ReviewTable from "./ReviewTable.js";
+import GraphSpace from "./GraphSpace";
+import { getAssignments } from "../../../../../api/getAssignment";
+import { useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+
+function AssignmentReview() {
+  const { loading, error, data } = useQuery(getAssignments);
+
+  if (loading) return "Loading...";
+
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <div class="container">
+      <h1>Assignment Review</h1>
+      <ReviewTable
+        labels={[
+          "Assignment Name",
+          "Your Grade",
+          "Median Grade",
+          "Max Grade",
+          "Min Grade",
+        ]}
+        data={[
+          data.assignments.map((d) => (
+            <Link
+              to={{
+                pathname: "/professor/assignment/review/" + d._id + "/",
+              }}
+            >
+             {d.name}
+            </Link>
+          )),
+          data.assignments.map((d) => {
+            if (d.mySubmission == null) {
+              return null;
+            } else {
+              return d.mySubmission.grade;
+            }}),
+          data.assignments.map((d) => {
+            if (!d.submissions.length || !d.submissions) {
+              return null;
+            } else {
+              return [getMedian(getGradeList(d.submissions))];
+            }
+          }),
+          data.assignments.map((d) => {
+            if (!d.submissions.length || !d.submissions) {
+              return null;
+            } else {
+              return [getMax(getGradeList(d.submissions))];
+            }
+          }),
+          data.assignments.map((d) => {
+            if (!d.submissions.length || !d.submissions) {
+              return null;
+            } else {
+              return [getMin(getGradeList(d.submissions))];
+            }
+          }),
+        ]}
+      />
+      <h1>Distribution</h1>
+      <GraphStyle>
+        <GraphSpace
+          data={data.assignments.map((a) => getGradeList(a.submissions))}
+          labels={data.assignments.map((a) => a.name)}
+        />
+      </GraphStyle>
+    </div>
+  );
+}
+const GraphStyle = styled.div`
+  padding: 20px;
+  border: 2px dashed #ced5d9;
+  margin: 30px;
+`;
+export default AssignmentReview;
