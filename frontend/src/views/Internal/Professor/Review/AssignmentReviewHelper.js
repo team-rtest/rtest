cd3bf98f5deb845713ccd4b27cf0508e767efdc2
jfrumk <64040663+jfrumk@users.noreply.github.com>
2020-11-20 20:54:07 -0500

    Fix merges (#205)
    * Fix merge issues
    
    * Fix dashboard and connect with assignment page

---
+++ frontend/src/views/Internal/Professor/Review/AssignmentReviewHelper.js
@@ -0,0 +1,21 @@
+import { median,max, min, mean} from 'mathjs';
+
+export function getGradeList(submissions) {
+    return submissions.map(submission => submission.grade);
+}
+
+export function getMedian(gradeList) {
+    return median(gradeList);
+}
+
+export function getMax(gradeList) {
+    return max(gradeList);
+}
+
+export function getMin(gradeList) {
+    return min(gradeList);
+}
+
+export function getMean(gradeList) {
+    return mean(gradeList);
+}
